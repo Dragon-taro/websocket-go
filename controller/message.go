c435c3857cc1bc377bd50d826bddfbf28a680dc1
Ryotaro Miyagwa <o_o_o_o_o1031@yahoo.co.jp>
2018-09-10 02:25:15 +0900

    message全件取得するapi完成

--- controller/message.go
+++ controller/message.go
@@ -1,13 +1,19 @@
 package controller
 
 import (
+	"fmt"
+
+	"github.com/Dragon-taro/websocket-go/model"
 	"github.com/gin-gonic/gin"
 )
 
 type MessageController struct{}
 
 func (m *MessageController) Get(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"masseage": "hello golang",
-	})
+	messages, err := model.GetAll()
+	if err != nil {
+		fmt.Println(err)
+		return // error処理分からん
+	}
+	c.JSON(200, messages)
 }
